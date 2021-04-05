5d2dcf158d45928d81161e0bd15fc5f2f50f30ca
Ollie <ollie.spear3@gmail.com>
2021-04-05 10:43:32 +0100

    Replaced default CreateDefaultBuilder method to use secrets.json

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace gamenvy
@@ -13,14 +14,29 @@ namespace gamenvy
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = new HostBuilder()
+            .ConfigureAppConfiguration((hostContext, builder) =>
+            {
+                builder.AddJsonFile("appsettings.json", false);
+                builder.AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", false);
+                if(hostContext.HostingEnvironment.IsDevelopment())
+                    builder.AddUserSecrets<Startup>();
+                builder.AddEnvironmentVariables();
+            }).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            })
+            .Build();
+
+            host.Run();
+           // CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        //public static IHostBuilder CreateHostBuilder(string[] args) =>
+        //    Host.CreateDefaultBuilder(args)
+        //        .ConfigureWebHostDefaults(webBuilder =>
+        //        {
+        //            webBuilder.UseStartup<Startup>();
+        //        });
     }
 }
---
+++ SharedObjects/APIContexts/APIKeys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedObjects.APIContexts
+{
+    public class APIKeys
+    {
+        public string RiotApiKey { get; set; }
+        public string BattleNetApiKey { get; set; }
+    }
+}
